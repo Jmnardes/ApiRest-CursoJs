1d037f652fe04145610b6e96e714a66e602a98a9
jmnardes <jmnardes@hotmail.com>
2020-09-23 15:10:00 -0400

    server and app modified

---
+++ dist/database/seeds/20200922140158-cria-usuario.js
@@ -0,0 +1,35 @@
+"use strict";const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: 'John Doe',
+          email: 'john@mail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Loli Doe',
+          email: 'loli@mail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Mark Doe',
+          email: 'mark@mail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  down: () => {},
+};
---
+++ dist/routes/userRoutes.js
@@ -0,0 +1,25 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
+var _UserController = require('../controllers/UserController'); var _UserController2 = _interopRequireDefault(_UserController);
+
+// middlewares
+var _loginRequired = require('../middlewares/loginRequired'); var _loginRequired2 = _interopRequireDefault(_loginRequired);
+
+const router = new (0, _express.Router)();
+
+// não deveria exisitr (só exemplos de CRUD)
+// router.get('/', loginRequired, userController.index);
+// router.get('/:id', userController.show);
+
+router.post('/', _UserController2.default.store);
+// router.put('/', loginRequired, userController.update);
+router.delete('/', _loginRequired2.default, _UserController2.default.delete);
+
+exports. default = router;
+
+/*
+index -> lista os usuarios -> GET
+store/create -> cria um novo usuario -> POST
+delete -> apaga um usuario -> DELETE
+show -> mostra um usuario -> GET
+update -> atualiza um usuario -> PATCH ou PUT
+*/
